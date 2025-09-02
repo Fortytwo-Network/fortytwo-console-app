19998e89f488d69b983e265d9038e5baa00712b4
AIvashov <ivashov.aleksey@proton.me>
2025-09-02 14:21:59 +0300

    Fixed llm(EXAONE) filename

--- macos.sh
+++ macos.sh
@@ -510,7 +510,7 @@ case $NODE_CLASS in
         ;;
     9)
         LLM_HF_REPO="LGAI-EXAONE/EXAONE-4.0-32B-GGUF"
-        LLM_HF_MODEL_NAME="LGAI-EXAONE_EXAONE-4.0-32B-Q4_K_M.gguf"
+        LLM_HF_MODEL_NAME="EXAONE-4.0-32B-Q4_K_M.gguf"
         NODE_NAME="⬢ ADVANCED GENERALIST: EXAONE 4.0 32B Q4"
         ;;
     10)
